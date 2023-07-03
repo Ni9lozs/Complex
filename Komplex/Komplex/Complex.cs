288ed3ab7cc2ca93dd9a4fee266cfc55758a311b
Ni9lozs <137774639+Ni9lozs@users.noreply.github.com>
2023-07-03 17:07:48 +0200

    Add files via upload

---
+++ Komplex/Komplex/Complex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komplex
+{
+    public class IllegalException : Exception { }
+    internal class Complex
+    {
+        private double re;
+        private double im;
+
+        public Complex(double re, double im) { 
+            this.re = re;
+            this.im = im;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            str += re;
+            str += "+";
+            str += im;
+            str += "i";
+            return str;
+        }
+
+        public static Complex operator+(Complex c1, Complex c2) {
+            Complex c = new Complex(c1.re + c2.re, c1.im + c2.im);
+            return c;
+        }
+
+        public static Complex operator-(Complex c1, Complex c2) {
+            Complex c = new Complex(c1.re - c2.re, c1.im - c2.im);
+            return c;
+        }
+        public static Complex operator*(Complex c1, Complex c2) {
+            Complex c = new Complex((c1.re*c2.re) - (c1.im*c2.im), (c1.re*c2.im) + (c1.im*c2.re));
+            return c;
+        }
+        public static Complex operator/(Complex c1, Complex c2) {
+            if (c2.re == 0 && c2.im == 0) {
+                throw new IllegalException();
+            }
+            Complex c = new Complex(((c1.re * c2.re)+(c1.im * c2.im))/((c2.re*c2.re)
+                +(c2.im*c2.im)),((c1.im*c2.re)-(c1.re*c2.im))/
+                ((c2.re * c2.re) +(c2.im * c2.im)));
+            return c;
+        }
+            
+    }
+}
---
+++ Komplex/Komplex/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+namespace Komplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.menu();
+        }
+    }
+
+    class Menu {
+        private readonly Complex[] c = new Complex[2];
+
+        public void menu()
+        {
+            int menu;
+            do {
+                menuWrite();
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        Print();
+                        break;
+                    case 2:
+                        Sum();
+                        break;
+                    case 3:
+                        Sub();
+                        break;
+                    case 4:
+                        Mul();
+                        break;
+                    case 5:
+                        Div();
+                        break;
+                }
+            } while (menu != 0);
+        }
+
+        public Complex complexRead(Complex c) {
+            bool jo;
+            double re, im;
+            Console.WriteLine("Adja meg a komplex szám valós részét: ");
+            do
+            {
+               jo = double.TryParse(Console.ReadLine(), out re);
+            } while (!jo);
+
+            Console.WriteLine("Adja meg a komplex szám imaginárius részét: ");
+            do
+            {
+                jo = double.TryParse(Console.ReadLine(), out im);
+            } while (!jo);
+            c = new Complex(re, im);
+            return c;
+        }
+
+        public void menuWrite() {
+            Console.WriteLine("Válassza ki, mit szeretne csinálni.");
+            Console.WriteLine("0 - kilépés a programból");
+            Console.WriteLine("1 - Komplex szám kiírása");
+            Console.WriteLine("2 - Komplex számok összeadása");
+            Console.WriteLine("3 - Két komplex szám kivonása");
+            Console.WriteLine("4 - Komplex számok szorzata");
+            Console.WriteLine("5 - Két komplex szám osztása");
+        }
+
+        public void Print() {
+            c[0] = complexRead(c[0]);
+            Console.WriteLine(c[0].ToString());
+        }
+
+        public void Sum() {
+            c[0] = complexRead(c[0]);
+            c[1] = complexRead(c[1]);
+            Console.WriteLine((c[0] + c[1]).ToString());
+        }
+
+        public void Sub() {
+            Console.WriteLine("kisebbítendő: ");
+            c[0] = complexRead(c[0]);
+            Console.WriteLine("kivonandó: ");
+            c[1] = complexRead(c[1]);
+            Console.WriteLine((c[0] - c[1]).ToString());
+        }
+
+        public void Mul() {
+            c[0] = complexRead(c[0]);
+            c[1] = complexRead(c[1]);
+            Console.WriteLine((c[0] * c[1]).ToString());
+        }
+        
+        public void Div()
+        {
+            Console.WriteLine("Osztandó: ");
+            c[0] = complexRead(c[0]);
+            Console.WriteLine("Osztó: ");
+            c[1] = complexRead(c[1]);
+            Console.WriteLine((c[0] / c[1]).ToString());
+        }
+
+    }
+}
